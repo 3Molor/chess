3aeb8df363fc4fceff48b369c764e2bdd100e46f
ZR <2016154785@qq.com>
2023-05-22 19:16:11 +0800

    改进了一下的计算器

--- computer.java
+++ computer.java
@@ -1,166 +1,122 @@
-
 import java.awt.*;
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-public class computer extends JFrame implements ActionListener {
+/**
+ * computer
+ */
+public class computer extends JFrame {
 
-    JPanel jp1, jp2;
-    JTextField jtf;
-    JLabel jl;
-    JButton jbn1, jbn2, jbn3, jbn4, jbn5, jbn6, jbn7, jbn8, jbn9, jbn0;
-    JButton jba, jbs, jbm, jbd, jbe, jbc;
-
-    double num1 = 0;
-    double num2 = 0;
+    String[] jparrs = { "%", "CE", "C", "backspace", "1/x", "x^2", "2√x", "÷", "7", "8", "9", "×", "4", "5", "6", "-",
+            "1", "2", "3", "+", "±", "0", ".", "=" };
+    JTextField textField;
+    JButton[] buttons;
     double result = 0;
-    double num3 = 0;
-    int op = 0;
+    String operator = "="; // 运算符
+    boolean start = true; // 是否开始输入
 
-    public computer() {
+    public computer(String title) {
+        super(title);
+        setSize(335, 510);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        // setResizable(false);
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(6, 4, 5, 5));
 
-        jp1 = new JPanel();
-        jtf = new JTextField(20);
-        jtf.setHorizontalAlignment(JTextField.RIGHT);
-        jp1.add(jtf);
+        buttons = new JButton[jparrs.length];
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton(jparrs[i]);
+            buttons[i].setSize(80, 70);
+            buttons[i].addActionListener(new MyActionListener());
+            jp.add(buttons[i]);
+        }
+        textField = new JTextField("0") {
+            public void setPreferredSize(Dimension preferredSize) {
+                textField.setPreferredSize(new Dimension(50, 50));
+            }
+        };
+        textField.setHorizontalAlignment(JTextField.RIGHT);
+        textField.setEditable(false);
 
-        jl = new JLabel();
-        jp1.add(jl);
-
-        jp2 = new JPanel();
-        jp2.setLayout(new GridLayout(4, 4, 8, 8));
-        // 添加数字键
-        jbn1 = new JButton("1");
-        jbn1.addActionListener(this);
-        jp2.add(jbn1);
-        jbn2 = new JButton("2");
-        jbn2.addActionListener(this);
-        jp2.add(jbn2);
-        jbn3 = new JButton("3");
-        jbn3.addActionListener(this);
-        jp2.add(jbn3);
-        jba = new JButton("+");
-        jba.addActionListener(this);
-        jp2.add(jba);
-        jbn4 = new JButton("4");
-        jbn4.addActionListener(this);
-        jp2.add(jbn4);
-        jbn5 = new JButton("5");
-        jbn5.addActionListener(this);
-        jp2.add(jbn5);
-        jbn6 = new JButton("6");
-        jbn6.addActionListener(this);
-        jp2.add(jbn6);
-        jbs = new JButton("-");
-        jbs.addActionListener(this);
-        jp2.add(jbs);
-        jbn7 = new JButton("7");
-        jbn7.addActionListener(this);
-        jp2.add(jbn7);
-        jbn8 = new JButton("8");
-        jbn8.addActionListener(this);
-        jp2.add(jbn8);
-        jbn9 = new JButton("9");
-        jbn9.addActionListener(this);
-        jp2.add(jbn9);
-
-        // 添加 加减乘除等于清零
-
-        jbm = new JButton("*");
-        jbm.addActionListener(this);
-        jp2.add(jbm);
-        jbd = new JButton("/");
-        jbd.addActionListener(this);
-        jp2.add(jbd);
-        jbn0 = new JButton("0");
-        jbn0.addActionListener(this);
-        jp2.add(jbn0);
-        jbe = new JButton("=");
-        jbe.addActionListener(this);
-        jp2.add(jbe);
-        jbc = new JButton("C");
-        jbc.addActionListener(this);
-        jp2.add(jbc);
-        // 设置窗口属性
-        this.setTitle("计算器");
-        this.add(jp1, BorderLayout.NORTH);
-        this.add(jp2, BorderLayout.CENTER);
-
-        this.setSize(335, 510);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        getContentPane().add(jp, BorderLayout.SOUTH);
+        getContentPane().add(textField, BorderLayout.NORTH);
+        setVisible(true);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JButton btn = (JButton) e.getSource();
-        String input = btn.getText();
-
-        if (input.equals("+")) {
-            num1 = Double.parseDouble(jtf.getText());
-            jtf.setText("");
-            op = 1;
-            jl.setText(String.valueOf(result));
-            result = num3 + num1;
-        } else if (input.equals("-")) {
-            num1 = Double.parseDouble(jtf.getText());
-            jtf.setText("");
-            op = 2;
-            result = num3 - num1;
-            jl.setText(String.valueOf(result));
-        } else if (input.equals("*")) {
-            num1 = Double.parseDouble(jtf.getText());
-            jtf.setText("");
-            op = 3;
-            result = num3 * num1;
-            jl.setText(String.valueOf(result));
-        } else if (input.equals("/")) {
-            num1 = Double.parseDouble(jtf.getText());
-            jtf.setText("");
-            op = 4;
-            result = num3 / num1;
-            jl.setText(String.valueOf(result));
-        } else if (input.equals("C")) {
-            num1 = 0;
-            num2 = 0;
-            num3 = 0;
-            result = 0;
-            jtf.setText("");
-            op = 0;
-            jl.setText(String.valueOf(result));
-        } else if (input.equals("=")) {
-            num2 = Double.parseDouble(jtf.getText());
-            switch (op) {
-                case 1:
-                    result = result + num2;
-                    break;
-                case 2:
-                    result = result - num2;
-                    break;
-                case 3:
-                    result = result * num2;
-                    break;
-                case 4:
-                    result = result / num2;
-                    break;
-                default:
-                    result = 0;
-                    break;
+    class MyActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String input = e.getActionCommand();
+            System.out.println(input);
+            if (".0123456789".indexOf(input) >= 0) {
+                if (start) {
+                    textField.setText(input);
+                } else {
+                    textField.setText(textField.getText() + input);
+                }
+                start = false;
+            } else if (input.equals("CE")) {
+                textField.setText("0");
+                start = true;
+            } else if (input.equals("C")) {
+                textField.setText("0");
+                start = true;
+                result = 0;
+            } else if (input.equals("backspace")) {
+                textField.setText(textField.getText().substring(0, textField.getText().length()-1));
+            } else{
+                if (start) {
+                    if (input.equals("-")) {
+                        textField.setText(input);
+                        start = false;
+                    } else {
+                        operator = input;
+                    }
+                } else {
+                    double x = Double.parseDouble(textField.getText());
+                    Calculate(x);
+                    operator = input;
+                    start = true;
+                }
             }
-            jtf.setText(String.valueOf(result));
-        } else {
-            String sc = jtf.getText();
-            sc += input;
-            jtf.setText(sc);
-            jl.setText(String.valueOf(result));
         }
 
-        num3 = num1;
-
-    }
-
-    public static void main(String[] args) {
-        new computer();
+        private void Calculate(double x) {
+            switch (operator) {
+                case "+":
+                    result += x;
+                    break;
+                case "-":
+                    result -= x;
+                    break;
+                case "×":
+                    result *= x;
+                    break;
+                case "÷":
+                    result /= x;
+                    break;
+                case "%":
+                    result %= x;
+                    break;
+                case "±":
+                    result = 0 - x;
+                    break;
+                case "1/x":
+                    result = 1 / x;
+                    break;
+                case "x^2":
+                    result = Math.pow(x, 2);
+                    break;
+                case "2√x":
+                    result = Math.sqrt(x);
+                    break;
+                case "=":
+                    result = x;
+                    break;
+            }
+                textField.setText("" + result);
+        }
     }
 
 }
---
+++ demo.java
@@ -0,0 +1,5 @@
+public class demo {
+    public static void main(String[] args) {
+        new computer("测试窗口");
+    }
+}
